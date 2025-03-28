eb4bd9dcea0dcf4825da39102d7e244b509d40d1
muzykantov.kd2741 <muzykantov.kd2741@MAST-407-L-1-R>
2025-03-28 13:18:43 +0500

    Добавьте файлы проекта.

---
+++ OkeiDormitory/Controllers/Mvc/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OkeiDormitory.Controllers.Mvc
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
